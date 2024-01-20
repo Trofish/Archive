7f29e9691a6dc243faed8826d1c5f8ef5e50d167
Yong Peng <yong.peng@wdc.com>
2024-01-20 16:43:02 +0800

    Nornir update.

---
+++ Nornir/test2.py
@@ -0,0 +1,13 @@
+from Two_FA_Nornir import *
+import getpass
+
+excel_file = "c:/script/My_Inventory.xlsx"
+commands = "c:/script/Nornir_LAB/show_cmd.txt"
+
+device_list = ExcelReader.read_excel(excel_file)
+
+uid = input("Please input login ID:")
+pwd = getpass.getpass("ID Password for login:")
+
+work = TwoFactorAuth(device_list=device_list, commands = commands, username=uid, password=pwd)
+work.run()
